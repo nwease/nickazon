360a678ad908ab43dab3cdb7e94a109f8ca89b5c
nwease <nickwease@gmail.com>
2020-09-14 12:14:47 +0200

    Worked on Checkout and Subtotal components.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/Checkout.css';
+import Subtotal from './Subtotal';
 
 const Checkout = () => {
     return (
@@ -10,16 +11,22 @@ const Checkout = () => {
                     src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
                     alt='Link'
                 />
-            </div>
 
-            <div>
-                <h2 className='checkout__title'>
-                    Your Shopping Basket
-                </h2>
+                <div>
+                    <h2 className='checkout__title'>
+                        Your Shopping Basket
+                    </h2>
+
+                    {/*BASKETITEM*/}
+                    {/*BASKETITEM*/}
+                    {/*BASKETITEM*/}
+                    {/*BASKETITEM*/}
+                    {/*BASKETITEM*/}
+                </div>
             </div>
 
             <div className='checkout__right'>
-
+                <Subtotal />
             </div>
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import '../css/Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='header'>
-            <img
-                className='header__logo'
-                src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
-                alt='Amazon Logo'
-            />
+            <Link to='/'>
+                <img
+                    className='header__logo'
+                    src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                    alt='Amazon Logo'
+                />
+            </Link>
 
             <div className='header__search'>
                 <input className='header__input' type='text'/>
@@ -49,13 +52,15 @@ const Header = () => {
                     </span>
                 </div>
 
-                <div className='header__optionBasket'>
-                    <ShoppingBasketIcon />
+                <Link to='/checkout'>
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
 
-                    <span className='header__option2 header__basketCount'>
-                        0
-                    </span>
-                </div>
+                        <span className='header__option2 header__basketCount'>
+                            0
+                        </span>
+                    </div>
+                </Link>
             </div>
         </div>
     );
