95246ae756bafd4d71a7a98953c41961c122cbd5
nwease <nickwease@gmail.com>
2020-09-22 13:27:32 +0200

    More changes.

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/nickazon-618aa/us-central1/api' //THE API URL (CLOUD FUNCTION)
+    baseURL: 'https://us-central1-nickazon-618aa.cloudfunctions.net/api' //THE API URL (CLOUD FUNCTION)
+    //  http://localhost:5001/nickazon-618aa/us-central1/api
 });
 
 export default instance;
